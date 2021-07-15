71a77a4fc14a2b751058392f2a3817febf873575
Huu Loc <78517332+huynhhuuloc129@users.noreply.github.com>
2021-07-15 10:56:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -92,14 +92,16 @@ function xuli(inputvalue) {
     // Check coi có học phần nào đã add không
     var childlist = document.getElementsByClassName("child");
     var buttonlist = document.getElementsByClassName("childbutton");
+    // Chạy 1 dòng trong dãy thứ 2 tới chủ nhật
     for (let i = 2; i <= 8; i++) {
         checkbutton("t" + i, inputvalue.toUpperCase());
         var ma = checkhp("t" + i, inputvalue.toUpperCase());
         if (ma != null) {
-            var malop = ma.slice(ma.indexOf(" ") + 1, ma.length);
 
+            var malop = ma.slice(ma.indexOf(" ") + 1, ma.length);
+            // Chạy 1 dòng trong buttonlist
             for (let j = 0; j < buttonlist.length; j++) {
-                if (childlist[1 + 11 * j].innerHTML == malop) {
+                if (childlist[0 + 11 * j].innerHTML == malop) {
                     buttonlist[j].innerHTML = "Delete";
                     buttonlist[j].style.background = "grey";
                 } else buttonlist[j].disabled = true;
@@ -116,18 +118,31 @@ function checkbutton(thu, inputvalue) {
     // chạy dòng for từ thứ 2 đến chủ nhật
     for (let i = 0; i < thulist.length; i++) {
         if (thulist[i].innerHTML != "" && thulist[i].innerHTML.indexOf(inputvalue) == -1) {
+            // Chạy dòng trong 1 list học phần để kiểm tra có nút delete ko
+            var t = false;
+            for (let j = 0; j < buttonlist.length; j++) {
+                if (buttonlist[j].innerHTML == "Delete") {
+                    t = true;
+                }
+            }
+            if (t) {
+                for (let j = 0; j < buttonlist.length; j++) {
+                    if (buttonlist[j].innerHTML != "Delete") buttonlist[j].disabled=true;
+                }
+            } else{
             // Chạy dòng trong 1 list học phần
+
             for (let j = 0; j < buttonlist.length; j++) {
                 let sotiet = parseInt(childlist[5 + 11 * j].innerHTML);
                 let thuhp = parseInt(childlist[6 + 11 * j].innerHTML);
                 let tietbd = parseInt(childlist[7 + 11 * j].innerHTML);
                 // Kiem tra coi có trùng ngày, tiết hay không
                 if (thu[1] == thuhp && (tietbd + sotiet) >= (i + 1) && (i <= 4 && tietbd <= (i + 1) || (i >= 6) && (tietbd <= i))) {
-                    let malop = childlist[1 + 11 * j].innerHTML;
+                    let malop = childlist[0 + 11 * j].innerHTML;
 
                     // Chạy dòng for check coi có ai trùng mã lớp thì cho disable luôn
                     for (let k = 0; k < buttonlist.length; k++) {
-                        if (malop == childlist[1 + 11 * k].innerHTML) {
+                        if (malop == childlist[0 + 11 * k].innerHTML) {
                             buttonlist[k].disabled = true;
                             buttonlist[k].className += " block";
                         }
@@ -138,6 +153,7 @@ function checkbutton(thu, inputvalue) {
                 }
             }
         }
+    }
     }
 }
 
@@ -178,14 +194,15 @@ function xulibutton() {
     const buttonclicked = this;
     var classname = this.className;
     var i = classname.match(/(\d+)/)[0];
-    let tenlop = childlist[1 + 11 * (i - 1)].innerHTML;
+    let tenlop = childlist[0 + 11 * (i - 1)].innerHTML;
     // Đổi add sang delete
     if (buttonclicked.innerHTML == "Add") {
         for (let j = 0; j < buttonlist.length; j++) {
             buttonlist[j].disabled = true;
         }
         for (let j = 0; j < buttonlist.length; j++) {
-            if (childlist[1 + 11 * j].innerHTML == tenlop) {
+            if (childlist[0 + 11 * j].innerHTML == tenlop) {
+
                 let sotiet = parseInt(childlist[5 + 11 * j].innerHTML);
                 let thu = parseInt(childlist[6 + 11 * j].innerHTML);
                 let tietbd = parseInt(childlist[7 + 11 * j].innerHTML);
@@ -193,14 +210,24 @@ function xulibutton() {
                 buttonlist[j].disabled = false;
                 buttonlist[j].innerHTML = "Delete";
                 buttonlist[j].style.background = "grey";
+                var vitri = buttonlist[j].className.match(/(\d+)/)[0];
+                let wholeline = document.getElementsByClassName("child" + vitri);
+                for (let k = 0; k < wholeline.length; k++) {
+                    wholeline[k].style.color = "black";
+                }
                 themvaoTKB(thu, sotiet, tietbd, mahp, tenlop);
             }
         }
     } else { // Đổi delete sang add
         for (let j = 0; j < buttonlist.length; j++) {
-            if (childlist[1 + 11 * j].innerHTML == tenlop) {
+            if (childlist[0 + 11 * j].innerHTML == tenlop) {
                 buttonlist[j].innerHTML = "Add";
                 buttonlist[j].style.background = "rgb(231, 231, 231)";
+                var vitri = buttonlist[j].className.match(/(\d+)/)[0];
+                let wholeline = document.getElementsByClassName("child" + vitri);
+                for (let k = 0; k < wholeline.length; k++) {
+                    wholeline[k].style.color = "brown";
+                }
             }
             if (buttonlist[j].className.indexOf("block") < 0) buttonlist[j].disabled = false;
         }
@@ -214,8 +241,8 @@ function themvaoTKB(thu, sotiet, tietbd, mahp, tenlop) {
     var thulist = document.getElementsByClassName("t" + thu);
     for (let i = tietbd; i < tietbd + sotiet; i++) {
         if (tietbd < 6)
-            thulist[i - 1].innerHTML = mahp + " " + tenlop;
-        else thulist[i].innerHTML = mahp + " " + tenlop;
+            thulist[i - 1].innerHTML = mahp + "<br> " + tenlop;
+        else thulist[i].innerHTML = mahp + "<br> " + tenlop;
     }
 }
 
